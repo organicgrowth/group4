64d0c8d2bd40383d12466b067c30eb9eacce713f
EnnyHellsen <enny.hellsen.j@gmail.com>
2016-12-08 13:18:26 +0100

    ennys change nr2

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+  $("#scroll_down").click(function() {
+      $('html, body').animate({
+          scrollTop: $("#space").offset().top
+        }, 2000);
+      });
+
+});
